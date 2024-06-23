d83ca6f33740312d1031cc66fdfe95193a7981c6
kushana <rashmisenadheera98@gmail.com>
2024-06-23 14:56:04 +0530

    Set feature to fetch data from backend and display on front end

--- server.js
+++ server.js
@@ -3,7 +3,6 @@ const cors = require('cors');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const advertisementRoutes = require('./routes/advertismentRoutes');
-const data_product = require('./data');
 
 const app = express();
 const port = process.env.PORT || 4000;
@@ -20,9 +19,9 @@ mongoose.connect('mongodb+srv://kushana:Izn5o0iC865jB8yg@cluster0.erqc8h4.mongod
 
 app.use('/api', advertisementRoutes);
 
-app.get('/displayData', (req, res) => {
+{/*app.get('/displayData', (req, res) => {
   res.json(data_product);
-});
+});*/}
 
 app.listen(port, () => {
   console.log(`Server listening on port ${port}`);
