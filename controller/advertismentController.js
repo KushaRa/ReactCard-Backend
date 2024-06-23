f0ab8669db20510eecac060f5481ee693bd8c235
kushana <rashmisenadheera98@gmail.com>
2024-06-23 11:05:56 +0530

    Set path for displaying data

--- controller/advertismentController.js
+++ controller/advertismentController.js
@@ -11,3 +11,12 @@ exports.submitAdvertisement = async (req, res) => {
     res.status(500).send('Error submitting post.');
   }
 };
+exports.displayAdvetisements = async(req, res) =>{
+  try{
+    const adds = await Advertisement.find();
+    res.json(adds);
+  }
+  catch(err){
+    res.status(500).json({message:err.message});
+  }
+};
--- routes/advertismentRoutes.js
+++ routes/advertismentRoutes.js
@@ -3,5 +3,6 @@ const router = express.Router();
 const advertisementController = require('../controller/advertismentController');
 
 router.post('/submit-add', advertisementController.submitAdvertisement);
+router.get('/display-add',advertisementController.displayAdvetisements);
 
 module.exports = router;
