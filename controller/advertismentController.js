4f55cd4ec0289b134e323b9ba5fab6dbc52d951d
kushana <rashmisenadheera98@gmail.com>
2024-06-25 20:46:03 +0530

    Set backend for create detailed card  for display data

--- controller/advertismentController.js
+++ controller/advertismentController.js
@@ -8,15 +8,31 @@ exports.submitAdvertisement = async (req, res) => {
     await newAdvertisement.save();
     res.status(201).send('Post submitted successfully!');
   } catch (err) {
+    console.error(err);
     res.status(500).send('Error submitting post.');
   }
 };
-exports.displayAdvetisements = async(req, res) =>{
-  try{
-    const adds = await Advertisement.find();
-    res.json(adds);
+
+exports.displayAdvertisements = async (req, res) => {
+  try {
+    const ads = await Advertisement.find();
+    res.json(ads);
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ message: err.message });
   }
-  catch(err){
-    res.status(500).json({message:err.message});
+};
+
+exports.getAdvertisementById = async (req, res) => {
+  const { id } = req.params;
+  try {
+    const ad = await Advertisement.findById(id);
+    if (!ad) {
+      return res.status(404).json({ message: 'Advertisement Not Found' });
+    }
+    res.json(ad);
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ message: err.message });
   }
 };
